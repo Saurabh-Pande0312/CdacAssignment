9ef44b7797ccefb04de74b0dccdddef47be3e816
Saurabh-Pande0312 <161593300+Saurabh-Pande0312@users.noreply.github.com>
2024-03-01 15:39:36 +0530

    Day 1 Logic Building Assignment Submitted

---
+++ Day1/Addition.java
@@ -0,0 +1,27 @@
+class Addition{
+	public  static void main(String arg[]){
+	/*
+		int number1 = 10;  //variable
+		int number2 = 20;
+		int add;
+		add = number1 + number2;
+		System.out.println("Addition of Number");
+		System.out.println(add);
+	*/
+	/*
+		float number1 = 10.0f;  
+		float number2 = 20;
+		float add;
+		add = number1 + number2;
+		System.out.println("Addition of Number");
+		System.out.println(add);	
+	*/
+		if(true){
+		System.out.println("in IF block");
+		}
+		else{System.out.println("in else block");
+		}	
+
+
+	}
+}
